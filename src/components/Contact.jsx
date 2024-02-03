4acf1500745fc79a100556976f932ab8dfbb8bc0
SuperMoooo <andremontoito@gmail.com>
2024-02-03 17:59:13 +0000

    Fix Mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,8 @@ export default function Contact() {
                 <h1 className="uppercase text-4xl italic">About me</h1>
                 <hr className="w-20 h-1 border-white border-1 hrGradient2 md:w-48 lg:w-72" />
             </div>
-            <div className=" bg-zinc-900 p-4 border  border-zinc-900 rounded-lg shadow space-y-2 flex items-center justify-center flex-col w-[28rem] md:w-[46rem] lg:w-[60rem] h-48">
-                <p className="text-xl italic text-center text-[#fed7aa] md:text-3xl lg:text-4xl">
+            <div className=" bg-zinc-900 p-4 border  border-zinc-900 rounded-lg shadow space-y-2 flex items-center justify-center flex-col w-80 md:w-[46rem] lg:w-[60rem] h-48">
+                <p className="text-xl italic text-center text-[#fed7aa] md:text-2xl lg:text-3xl">
                     If you wanna know more about me, hire me and lets talk ;D
                 </p>
             </div>
